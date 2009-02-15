7663cdceeadcc715b552a6be460d1105827a7e70
Matt Wood <mza@macbook.local>
2009-02-15 16:43:45 +0000

    Updated tests and output locations.

--- lib/launch-pad/command/haproxy.rb
+++ lib/launch-pad/command/haproxy.rb
@@ -1,6 +1,8 @@
+require 'erb'
+
 module LaunchPad
   class Haproxy < Command
-    
+      
     def activate(options = {})
       begin
         CreateNodes.migrate(:up)
@@ -16,8 +18,20 @@ module LaunchPad
     def rebuild_config(options = {})
       location = options[:with_templates]
       puts "Rebuilding Haproxy config with: #{location}"
-      nodes = Node.all
-      puts "Registered nodes: #{nodes.size}"      
+      template = ERB.new File.read("#{location}/haproxy.erb")
+      result = template.result(binding)
+      File.open("#{options[:to]}/haproxy.conf", 'w') do |f|
+        f.write result
+      end
+      puts "Config file saved"
+    end
+    
+    def servers_for(role, options = {})
+      response = ""
+      Node.all.each_with_index do |node,index|
+        response = response + "server  #{role}_#{options[:type]}_#{index} #{node.name} cookie app1inst1 check inter 2000 rise 2 fall 5\n"
+      end
+      response
     end
         
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,17 @@
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/turbo'
 require File.dirname(__FILE__) + '/../lib/launch-pad'
+
+module Test
+  module Unit 
+    class TestCase
+      
+      def assert_file_equal(file1, file2)
+        file1_contents = File.new(file1).read
+        file2_contents = File.new(file2).read
+        assert_equal file1_contents, file2_contents
+      end
+      
+    end
+  end
+end
